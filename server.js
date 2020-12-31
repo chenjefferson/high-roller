d5332c9d1d9e1d38175a72cf5e43ada07d38c925
Jefferson William Chen <jeffwilliamchen@ieee.org>
2020-12-31 15:56:07 -0800

    added user model

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
-
-const app = express();
+const connectDB = require('./config/db')
 
 /* how Heroku gets port */
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// connect dtabase
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => res.send('Hello!'))
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/users'));
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
