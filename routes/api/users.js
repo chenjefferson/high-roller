924f58c569f6b6dc476868d0c2e589a86adb15d3
Jefferson William Chen <jeffwilliamchen@ieee.org>
2021-01-01 17:05:44 -0800

    added JWT tokens to user path

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
 const User = require('../../models/User');
+const config = require('config');
 
 // @route   POST api/users
 // @desc    Register user
@@ -18,10 +19,10 @@ router.post(
         check('email', 'Please include a valid email')
             .isEmail(),
         check('password', 'Please enter a password with 6 or more characters')
-            .isLength({ min: 6 })
+            .isLength({ min: 6, max: 32 })
     ], async (req, res) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
+        if (! errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
@@ -55,12 +56,25 @@ router.post(
             user.password = await bcrypt.hash(password, salt);
 
             // save to DB
-            user.save();
-            res.send('User successfully created');
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
         } catch(err) {
             console.error(err.message);
             res.status(500).send('Server error');
-            return;
         }
     });
 
